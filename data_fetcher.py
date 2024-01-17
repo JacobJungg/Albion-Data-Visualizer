900e0ec1184e5c7a60d88e08d5150a79c8b88512
Jacob Jung1~ <jacobj@live.ca>
2024-01-16 21:17:17 -0500

    Update prices

--- data_fetcher.py
+++ data_fetcher.py
@@ -18,5 +18,21 @@ def fetch_data():
             DATA.append({"Location": location, "Item Name": item, "Price": price})
     print(DATA)
 
-    return DATA  # Add this line to return the data
+    return DATA
 
+def update_prices(selected_item):
+    price_outputs = []
+    selected_item_data = [item for item in DATA if item['Item Name'] == selected_item]
+
+    if selected_item_data:
+        price_outputs.append(f"Price: {selected_item_data[0]['Price']}")
+        for location in LOCATIONS:
+            location_data = next((item for item in selected_item_data if item['Location']== location), None)
+            if location_data:
+                price_outputs.append(location_data['Price'])
+            else:
+                price_outputs.append("Price: N/A")
+    else:
+        price_outputs = ["Price: N/A"] * (len(LOCATIONS) + 1)
+    print(price_outputs)
+    return price_outputs
